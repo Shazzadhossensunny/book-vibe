049188f68b2d9ed98f49a70d3086adaa3c1ae6d4
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-03-26 03:51:10 +0600

    show localstorge

---
+++ src/components/ReadBook/ReadBook.jsx
@@ -0,0 +1,66 @@
+import Tag from "../Tag/Tag";
+import { IoLocationOutline } from "react-icons/io5";
+import { HiOutlineUsers } from "react-icons/hi2";
+import { MdOutlineContactPage } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+export default function ReadBook({ read }) {
+  const {
+    image,
+    bookName,
+    author,
+    tags,
+    yearOfPublishing,
+    publisher,
+    totalPages,
+    category,
+    rating,
+  } = read;
+  return (
+    <div className="border border-[#13131399] rounded-2xl p-6 flex flex-col lg:flex-row gap-6 mt-8">
+      <div className="bg-[#1313130D] rounded-2xl p-7">
+        <img className="h-[172px] mx-auto rounded-2xl" src={image} alt="" />
+      </div>
+      <div className="flex-1">
+        <h3 className="text-[#131313] text-2xl font-bold">{bookName}</h3>
+        <p className="mt-4 text-[#131313CC] font-medium">By : {author}</p>
+        <div className="flex flex-wrap gap-4 items-center">
+          <div className="flex flex-wrap items-center gap-4">
+            <strong className="mt-5">Tag</strong>
+            {tags.map((tag, index) => (
+              <Tag key={index} tag={tag}></Tag>
+            ))}
+          </div>
+          <div className="flex items-center gap-2 mt-5">
+            <IoLocationOutline className="text-xl"></IoLocationOutline>
+            <p className="text-[#131313CC]">
+              Year of Publishing: {yearOfPublishing}
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-wrap items-center gap-4 mt-4">
+          <div className="flex items-center gap-2">
+            <HiOutlineUsers className="text-xl"></HiOutlineUsers>
+            <p className="text-[#131313CC]">Publisher: {publisher}</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <MdOutlineContactPage className="text-xl"></MdOutlineContactPage>
+            <p className="text-[#131313CC]">Page {totalPages}</p>
+          </div>
+        </div>
+        <div className="border border-[#13131326] my-4"></div>
+        <div className="space-x-3 space-y-3 lg:space-y-0">
+          <div className="badge bg-[#328eff26] p-4 text-[#328EFF]">
+            Category: {category}
+          </div>
+          <div className="badge bg-[#ffac3326] p-4 text-[#FFAC33]">
+            Rating: {rating}
+          </div>
+          <Link to={""} className="badge bg-[#23BE0A] p-4 text-white">
+            View Details
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/ListedBooks/ListedBooks.jsx
+++ src/pages/ListedBooks/ListedBooks.jsx
@@ -1,7 +1,61 @@
-
-
+import { useEffect, useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { getBooks, getRead } from "../../utility/localStorage";
+import ReadBook from "../../components/ReadBook/ReadBook";
+import WishlistBook from "../../components/WishlistBook/WishlistBook";
 export default function ListedBooks() {
+  const [readBooks, setReadBooks] = useState([]);
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const storedBooks = getRead();
+    setReadBooks(storedBooks);
+  }, []);
+  useEffect(() => {
+    const storedBooks = getBooks();
+    setBooks(storedBooks);
+  }, []);
   return (
-    <div>ListedBooks</div>
-  )
+    <div className="px-3 lg:px-0">
+      <div className="bg-[#1313130D] rounded-2xl p-8 text-center mt-8">
+        <h4 className="text-[#131313] text-2xl font-bold">Books</h4>
+      </div>
+      <div className="text-center mt-8">
+        <details className="dropdown">
+          <summary className="m-1 btn">Sort By</summary>
+          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </details>
+      </div>
+
+      <div className="mt-7">
+        <Tabs>
+          <TabList>
+            <Tab>Read Books</Tab>
+            <Tab>Wishlist Books</Tab>
+          </TabList>
+          <TabPanel>
+            <div>
+              {readBooks.map((read) => (
+                <ReadBook key={read.bookId} read={read}></ReadBook>
+              ))}
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div>
+              {books.map((book) => (
+                <WishlistBook key={book.bookId} book={book}></WishlistBook>
+              ))}
+            </div>
+          </TabPanel>
+        </Tabs>
+      </div>
+    </div>
+  );
 }
