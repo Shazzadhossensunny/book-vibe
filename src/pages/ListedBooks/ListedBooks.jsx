9dcc46eaab225b7223c523e7211ff03ba9d7f72c
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-03-26 15:45:54 +0600

    sort function complete

--- src/pages/ListedBooks/ListedBooks.jsx
+++ src/pages/ListedBooks/ListedBooks.jsx
@@ -4,17 +4,53 @@ import "react-tabs/style/react-tabs.css";
 import { getBooks, getRead } from "../../utility/localStorage";
 import ReadBook from "../../components/ReadBook/ReadBook";
 import WishlistBook from "../../components/WishlistBook/WishlistBook";
+import { IoIosArrowDown } from "react-icons/io";
 export default function ListedBooks() {
   const [readBooks, setReadBooks] = useState([]);
   const [books, setBooks] = useState([]);
+  const [displayJob, setDisplayJob] = useState([])
+  const [displayWishlist, setDisplayWishlist] = useState([])
   useEffect(() => {
     const storedBooks = getRead();
     setReadBooks(storedBooks);
+    setDisplayJob(storedBooks)
   }, []);
   useEffect(() => {
     const storedBooks = getBooks();
     setBooks(storedBooks);
+    setDisplayWishlist(storedBooks)
   }, []);
+  const handleFIlterRead = (filter) => {
+    if(filter === 'rating'){
+      const ratingFilter = readBooks.sort((a, b) => b.rating - a.rating);
+      setDisplayJob([...ratingFilter])
+    }
+    else if(filter === 'pages'){
+      const pagesByFilter = readBooks.sort((a, b) => b.totalPages - a.totalPages)
+      setDisplayJob([...pagesByFilter])
+    }
+    else if(filter === 'year'){
+      const yearByFilter = readBooks.sort((a, b) => b.yearOfPublishing - a.yearOfPublishing)
+      setDisplayJob([...yearByFilter])
+    }
+  }
+  const handleFIlterWishlist = (filter) => {
+    if(filter === 'rating'){
+      const ratingFilter = books.sort((a, b) => b.rating - a.rating);
+      setDisplayJob([...ratingFilter])
+    }
+    else if(filter === 'pages'){
+      const pagesByFilter = books.sort((a, b) => b.totalPages - a.totalPages)
+      setDisplayJob([...pagesByFilter])
+    }
+    else if(filter === 'year'){
+      const yearByFilter = books.sort((a, b) => b.yearOfPublishing - a.yearOfPublishing)
+      setDisplayJob([...yearByFilter])
+    }
+  }
+
+
+
   return (
     <div className="px-3 lg:px-0">
       <div className="bg-[#1313130D] rounded-2xl p-8 text-center mt-8">
@@ -22,15 +58,25 @@ export default function ListedBooks() {
       </div>
       <div className="text-center mt-8">
         <details className="dropdown">
-          <summary className="m-1 btn">Sort By</summary>
+          <summary className="m-1 btn bg-[#23BE0A] text-white hover:bg-[#23BE0A]">Sort By <IoIosArrowDown size={20}></IoIosArrowDown></summary>
           <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-            <li>
+            <li onClick={()=>{
+              handleFIlterRead('rating'),
+              handleFIlterWishlist('rating')
+            }
+           }>
               <a>Rating</a>
             </li>
-            <li>
+            <li onClick={()=>{
+              handleFIlterRead('pages')
+              handleFIlterWishlist('pages')
+            }}>
               <a>Number of pages</a>
             </li>
-            <li>
+            <li onClick={()=>{
+              handleFIlterRead('year')
+              handleFIlterWishlist('year')
+            }}>
               <a>Published year</a>
             </li>
           </ul>
@@ -45,14 +91,14 @@ export default function ListedBooks() {
           </TabList>
           <TabPanel>
             <div>
-              {readBooks.map((read) => (
+              {displayJob.map((read) => (
                 <ReadBook key={read.bookId} read={read}></ReadBook>
               ))}
             </div>
           </TabPanel>
           <TabPanel>
             <div>
-              {books.map((book) => (
+              {displayWishlist.map((book) => (
                 <WishlistBook key={book.bookId} book={book}></WishlistBook>
               ))}
             </div>
